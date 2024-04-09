960d2b0f91f71f57c8d85f14b1e889bcf4ca3460
Omar <omar.n.o.92@gmail.com>
2024-04-09 13:01:59 +0200

    styles-folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthWrapper } from "./context/auth.context.jsx";
 import App from "./App.jsx";
-import "./index.css";
+import "../src/Styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
